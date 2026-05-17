a35b799421b65a77fb445bee6e70b0afb426586e
kizzycpt <kizzycpt@proton.me>
2026-05-16 23:03:38 -0400

    small changes

--- variables/webscraper.py
+++ variables/webscraper.py
@@ -1,39 +1,30 @@
-import requests
-from bs4 import BeautifulSoup
-from playwright.sync_api import sync_playwright
+
+import requests, time
+
+from bs4                    import BeautifulSoup
+from playwright.sync_api    import sync_playwright, Page
+from pathlib                import Path
+from datetime               import datetime
+
+
+base_dir = Path(__file__).resolve().parent
+
 
 class WebScraper:
-    
-    
-    #--- Test Response to URl request ----------
-    def test_request(self):
-        try:
-            #url must include full format e.g (https://www.google.com)
-            url = input("Enter choice URL: ")
-            search = "prices"
-            
-            response = requests.get(url)
-
-            print(response.status_code)
-            print(response.headers)
-            print(response.text)
-
-            if search in response.text:
-                print("use beautiful soup to fetch RAW HTML data")
-            else:
-                print("[X] No data [X]\nUse playwright to render and move forward")
-
-
-        except Exception as e:
-            print("error {e}")
-
-    
+        
     def fetch_data(self):
+        
         
         try: 
             #Enter domain use domain and root lvl. e.g. (google.com or cia.gov or temple.edu or something.org/page )
             domain = str(input("Enter Domain: "))
             url = f"https://www.{domain}"
+
+            time_stamp = datetime.now().strftime("%Y_%B_%d_%H_%M%p")
+            log_path = base_dir / "peels" / f"{domain}" / f"{time_stamp}.json"
+            log_path.mkdir(parents=True, exist_ok=True)
+            
+
 
             response = requests.get(url)
 
@@ -51,9 +42,16 @@ class WebScraper:
         #Find out what framework is needed
         if search_flag in response.text:
             soup = BeautifulSoup(response.text, "html.parser")
-            print(soup.prettify())
 
+            with open(path, "a") as f:
+                f.write(soup.prettify())
+
+            print(soup.prettify())
+            log_path.write_text(soup.prettify)
+            
             soup.find(string=search_flag)
+
+            
 
         
         else:
@@ -73,6 +71,7 @@ class WebScraper:
                     print(soup.prettify())
                     soup.find(string=search_flag)
 
+        
 
             except Exception as e:
                 print(f"Error parsing data. {e}")
